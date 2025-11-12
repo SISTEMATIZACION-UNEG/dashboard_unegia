1a50030e259769147115a07479131e4ee38c1517
Acalcuriano <acalcurian671@gmail.com>
2025-11-12 13:55:11 -0400

    se ocultan los credenciales con archivo gitignore

--- conexion.py
+++ conexion.py
@@ -1,33 +1,23 @@
 import psycopg2
 
+from config import DATABASES
+
 # --- Conexión a base de datos de SEDES ---
 def obtener_conexion():
     try:
-        conexion = psycopg2.connect(
-            host="localhost",
-            user="postgres",
-            password="root123",
-            dbname="sedes_uneg",
-            port="5432",
-            client_encoding="UTF8"
-        )
+        db = DATABASES["sedes_uneg"]
+        conexion = psycopg2.connect(**db)
         return conexion
     except Exception as e:
-        print("Error al conectar a la base de datos sedes_uneg:", e)
+        print(" Error al conectar a la base de datos sedes_uneg:", e)
         return None
 
 
 # --- Conexión a base de datos de CATEGORÍAS y FALLAS ---
 def obtener_conexion_categorias():
     try:
-        conexion = psycopg2.connect(
-            host="localhost",
-            user="postgres",
-            password="root123",
-            dbname="categorias_fallas",
-            port="5432",
-            client_encoding="UTF8"
-        )
+        db = DATABASES["categorias_fallas"]
+        conexion = psycopg2.connect(**db)
         return conexion
     except Exception as e:
         print("Error al conectar a la base de datos categorias_fallas:", e)
@@ -37,14 +27,8 @@ def obtener_conexion_categorias():
 # --- Conexión a base de datos de REPORTES GENERALES ---
 def obtener_conexion_reportes_generales():
     try:
-        conexion = psycopg2.connect(
-            host="localhost",
-            user="postgres",
-            password="root123",   #  en tu código anterior pusiste "troot123" (error tipográfico)
-            dbname="reportes_generales",
-            port="5432",
-            client_encoding="UTF8"
-        )
+        db = DATABASES["reportes_generales"]
+        conexion = psycopg2.connect(**db)
         return conexion
     except Exception as e:
         print("Error al conectar a la base de datos reportes_generales:", e)
